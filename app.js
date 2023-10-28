6308b67e189c7e4e8dd50c78198c1c7b050abaf2
Rajesh-Choudhary <raj.cho@gmail.com>
2023-10-28 18:27:25 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,19 @@
+const PORT = process.env.PORT || 3001
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+    console.time('get')
+    const html = `<H1>Hello World RC!</H1>`;
+    try {
+        res.status(200).send(html.replace('{{output}}', ''))
+    } catch (error) {
+        console.error(error.message)
+        res.status(500).send("Error occured on server")
+    }
+    console.timeEnd('get')
+})
+
+app.listen(PORT, () => {
+    console.log(`Listening on PORT ${PORT}`)
+})
